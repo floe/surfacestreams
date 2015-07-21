5861540cc6a23aa439360de7182f91049d433f84
Florian Echtler <floe@butterbrot.org>
2015-07-21 21:31:16 +0200

    "modularize" protonect

--- protonect/Protonect.cpp
+++ protonect/Protonect.cpp
@@ -38,26 +38,22 @@
 
 bool protonect_shutdown = false;
 
+libfreenect2::Freenect2 freenect2;
+libfreenect2::Freenect2Device *dev = 0;
+libfreenect2::PacketPipeline *pipeline = 0;
+
+libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
+libfreenect2::FrameMap frames;
+libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+libfreenect2::Registration* registration = 0;
+
 void sigint_handler(int s)
 {
   protonect_shutdown = true;
 }
 
-int main(int argc, char *argv[])
+int setup(int argc, char* argv[])
 {
-  std::string program_path(argv[0]);
-  size_t executable_name_idx = program_path.rfind("Protonect");
-
-  std::string binpath = "/";
-
-  if(executable_name_idx != std::string::npos)
-  {
-    binpath = program_path.substr(0, executable_name_idx);
-  }
-
-  libfreenect2::Freenect2 freenect2;
-  libfreenect2::Freenect2Device *dev = 0;
-  libfreenect2::PacketPipeline *pipeline = 0;
 
   if(freenect2.enumerateDevices() == 0)
   {
@@ -122,10 +118,6 @@ int main(int argc, char *argv[])
   signal(SIGINT,sigint_handler);
   protonect_shutdown = false;
 
-  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
-  libfreenect2::FrameMap frames;
-  libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
-
   dev->setColorFrameListener(&listener);
   dev->setIrAndDepthFrameListener(&listener);
   dev->start();
@@ -133,29 +125,48 @@ int main(int argc, char *argv[])
   std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
   std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
 
-  libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+  registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+}
+
+void handle_frame() {
+  listener.waitForNewFrame(frames);
+  libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+  libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+  libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+
+  cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data));
+  cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+  cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+
+  registration->apply(rgb,depth,&undistorted,&registered);
+
+  cv::imshow("undistorted", cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data) / 4500.0f);
+  cv::imshow("registered", cv::Mat(registered.height, registered.width, CV_8UC4, registered.data));
+
+  int key = cv::waitKey(1);
+  protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
+
+  listener.release(frames);
+  //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
+}
+
+int main(int argc, char *argv[])
+{
+  std::string program_path(argv[0]);
+  size_t executable_name_idx = program_path.rfind("Protonect");
+
+  std::string binpath = "/";
+
+  if(executable_name_idx != std::string::npos)
+  {
+    binpath = program_path.substr(0, executable_name_idx);
+  }
+
+  setup(argc,argv);
 
   while(!protonect_shutdown)
   {
-    listener.waitForNewFrame(frames);
-    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
-    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
-
-    cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data));
-    cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
-    cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
-
-    registration->apply(rgb,depth,&undistorted,&registered);
-
-    cv::imshow("undistorted", cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data) / 4500.0f);
-    cv::imshow("registered", cv::Mat(registered.height, registered.width, CV_8UC4, registered.data));
-
-    int key = cv::waitKey(1);
-    protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
-
-    listener.release(frames);
-    //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
+    handle_frame();
   }
 
   // TODO: restarting ir stream doesn't work!
