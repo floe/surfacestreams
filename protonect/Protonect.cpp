b3a389f6b04806aa5250f401b7955cc640dbc0d1
Florian Echtler <floe@butterbrot.org>
2015-07-23 14:01:26 +0200

    works with depth filter!

--- protonect/Protonect.cpp
+++ protonect/Protonect.cpp
@@ -43,6 +43,8 @@
 
 #include <stdint.h>
 
+#include <immintrin.h>
+
 GstElement *gpipeline, *appsrc, *conv, *videosink;
 
 void buffer_destroy(gpointer data) {
@@ -113,7 +115,7 @@ libfreenect2::PacketPipeline *pipeline = 0;
 
 libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
 libfreenect2::FrameMap frames;
-libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4), bigdepth(1920,1080+2,4);
 libfreenect2::Registration* registration = 0;
 
 void sigint_handler(int s)
@@ -203,15 +205,36 @@ void handle_frame() {
   libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
   libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
 
-  registration->apply(rgb,depth,&undistorted,&registered);
+  // clip depth to max. 1m
+  float* depth_s = ((float*)depth->data);
+  float limit = 1000.0;
+  __m256 ymm0, ymm1, ymm2, ymm3 = _mm256_broadcast_ss(&limit);
+  for (int i = 0; i < (512*424)/8; i++, depth_s+=8) {
+    ymm0 = _mm256_load_ps(depth_s);
+    ymm1 = _mm256_cmp_ps(ymm0,ymm3,_CMP_LE_OS);
+    ymm2 = _mm256_and_ps(ymm0,ymm1);
+    _mm256_store_ps(depth_s,ymm2);
+    //if (*depth_s > 1000.0) *depth_s = 0;
+  }
+
+  registration->apply(rgb,depth,&undistorted,&registered,true,&bigdepth);
+
+  float* depth_p = ((float*)bigdepth.data)+1920;
+  unsigned int* rgb_src = (unsigned int*)rgb->data;
+  __m256 ymm4, ymm5 = _mm256_broadcast_ss(&limit);
+  for (int i = 0; i < (1920*1080)/8; i++, depth_p+=8, rgb_src+=8) {
+    ymm0 = _mm256_load_ps(depth_p);
+    ymm1 = _mm256_cmp_ps(ymm0,ymm5,_CMP_LE_OS);
+    ymm4 = (__m256)_mm256_load_si256((__m256i*)rgb_src);
+    ymm2 = _mm256_and_ps(ymm4,ymm1);
+    _mm256_store_si256((__m256i*)rgb_src,(__m256i)ymm2);
+  }
 
   prepare_buffer((GstAppSrc*)appsrc,rgb);
   g_main_context_iteration(g_main_context_default(),FALSE);
 
-  //listener.release(frames);
   delete depth;
   delete ir;
-  //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
 }
 
 int main(int argc, char *argv[])
