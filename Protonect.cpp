b410151ad964cc58f03782cc12e04d747f075b23
Florian Echtler <floe@butterbrot.org>
2017-05-16 09:55:57 +0200

    integrate gstreamer event handling & opencv

--- Protonect.cpp
+++ Protonect.cpp
@@ -41,18 +41,115 @@
 #include "viewer.h"
 #endif
 
+////////////////////////////////////////////////////////////////////////////////
+//
+// OpenCV stuff
+//
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+using namespace cv;
+
+std::vector<Point2f> src;
+std::vector<Point2f> dst;
+
+// im: identity matrix
+Mat im = (Mat_<float>(3,3) << 1280.0/1920.0, 0, 0, 0, 720.0/1080.0, 0, 0, 0, 1 );
+// pm: perspective matrix
+Mat pm = im;
+
+Mat calcPerspective() {
+
+  Mat result;
+
+  dst.push_back(Point2f(   0,  0));
+  dst.push_back(Point2f(1280,  0));
+  dst.push_back(Point2f(1280,720));
+  dst.push_back(Point2f(   0,720));
+
+  result = getPerspectiveTransform(src,dst);
+
+  src.clear();
+  dst.clear();
+
+  return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
 // GStreamer stuff
+//
 
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
+#include <gst/video/navigation.h>
 
 #include <stdint.h>
 #include <string.h>
 
 #include <immintrin.h>
 
+bool find_plane = true;
+bool filter = true;
+bool quit = false;
+
+float distance = 5;
+
 GstElement *gpipeline, *appsrc, *conv, *videosink;
+
+gboolean pad_event(GstPad *pad, GstObject *parent, GstEvent *event) {
+
+  if (GST_EVENT_TYPE (event) != GST_EVENT_NAVIGATION)
+    return gst_pad_event_default(pad,parent,event);
+
+  double x,y; int b;
+  const gchar* key;
+
+  switch (gst_navigation_event_get_type(event)) {
+
+    // calibration: top (left, right), bottom (left, right)
+    case GST_NAVIGATION_EVENT_MOUSE_BUTTON_RELEASE:
+      gst_navigation_event_parse_mouse_button_event(event,&b,&x,&y);
+      src.push_back(Point2f(1920.0*x/1280.0,1080.0*y/720.0));
+      break;
+
+    case GST_NAVIGATION_EVENT_KEY_PRESS:
+      gst_navigation_event_parse_key_event(event,&key);
+
+      // reset perspective matrix
+      if (key == std::string("space"))
+        pm = im;
+
+      // find largest plane
+      if (key == std::string("p"))
+        find_plane = true;
+
+      // subtract plane
+      if (key == std::string("f"))
+        filter = !filter;
+
+      // quit
+      if (key == std::string("q"))
+        quit = true;
+
+      // change plane distance threshold
+      if (key == std::string( "plus")) distance += 0.5;
+      if (key == std::string("minus")) distance -= 0.5;
+      break;
+
+    default:
+      return false;
+  }
+
+  if (src.size() == 4) {
+    Mat r = calcPerspective();
+    pm = r; //set_matrix(perspective,r.ptr<gdouble>(0));
+  }
+
+  return true;
+}
 
 void buffer_destroy(gpointer data) {
   libfreenect2::Frame* done = (libfreenect2::Frame*)data;
@@ -67,7 +164,6 @@ GstFlowReturn prepare_buffer(GstAppSrc* appsrc, libfreenect2::Frame* frame) {
   return gst_app_src_push_buffer(appsrc, buffer);
 }
 
-
 void gstreamer_init(gint argc, gchar *argv[]) {
 
   /* init GStreamer */
@@ -76,6 +172,10 @@ void gstreamer_init(gint argc, gchar *argv[]) {
   /* setup pipeline */
   gpipeline = gst_pipeline_new ("pipeline");
   appsrc = gst_element_factory_make ("appsrc", "source");
+
+  // attach event listener to appsrc pad
+  GstPad* srcpad = gst_element_get_static_pad (appsrc, "src");
+  gst_pad_set_event_function( srcpad, (GstPadEventFunction)pad_event );
 
   // create pipeline from string
   const char* pipe_desc = /*argv[2] ? argv[2] : */ "videoconvert ! fpsdisplaysink sync=false";
