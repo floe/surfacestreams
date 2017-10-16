dcbff6a644800f50321abd67add7160df79147e2
Florian Echtler <floe@butterbrot.org>
2017-10-16 17:53:08 +0200

    more scripts

---
+++ start_dual_src.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TARGET=${1:-127.0.0.1}
+SLOT=${2:-0}
+
+# need udev rules for the logitech cameras, e.g. in /etc/udev/rules.d/99-camera-symlink.rules
+FACECAM=/dev/video-face
+SURFCAM=/dev/video-surf
+
+# view/stream mjpeg directly from v4l2 device
+# note: needs to run BEFORE protonect, otherwise no USB bandwidth left ("no space left on device")
+gst-launch-1.0 -e v4l2src device=$FACECAM ! image/jpeg,width=1280,height=720,framerate=30/1 ! rtpgstpay config-interval=1 ! udpsink host=$TARGET port=$((6000+$SLOT)) &
+
+sleep 5
+
+SURFPIPE="rtpgstpay config-interval=1 ! udpsink host=127.0.0.1 port=$((5000+$SLOT))"
+
+if lsusb | grep -qi "045e:02d9" ; then
+	# Kinect connected
+	../build/bin/Protonect -gstpipe "$SURFPIPE"
+elif lsusb | grep -qi "045e:0775" ; then
+	# SUR40 connected
+	gst-launch-1.0 -e v4l2src device=$SURFCAM ! "video/x-raw,format=GRAY8,width=960,height=540" ! jpegenc ! $SURFPIPE
+else
+	./gstreamer $SURFCAM "$SURFPIPE"
+fi
+
