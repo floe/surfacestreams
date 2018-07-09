cc1a54eaa93fce7043f406c141fc0f5ca5161503
Florian Echtler <floe@butterbrot.org>
2018-07-09 15:01:00 +0200

    3 streams working

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-exec gst-launch-1.0 -vt videomixer name=mix ! videoconvert ! fpsdisplaysink sync=false \
+OUTPUT="fpsdisplaysink sync=false"
+#OUTPUT="x264enc ! mp4mux ! filesink location=test.mp4"
+exec gst-launch-1.0 -vte videomixer name=mix ! videoconvert ! $OUTPUT \
   udpsrc port=5001 caps=application/x-rtp ! rtpgstdepay ! jpegdec ! mix. \
-	udpsrc port=5002 caps=application/x-rtp ! rtpgstdepay ! jpegdec ! alpha method=green ! mix. \
+  udpsrc port=5002 caps=application/x-rtp ! rtpgstdepay ! jpegdec ! alpha method=green ! mix. \
   udpsrc port=5000 caps=application/x-rtp ! rtpgstdepay ! jpegdec ! alpha method=green ! mix.
