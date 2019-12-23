abd8d4131a341fab1cf98539bfdf9c446c1effc5
Florian Echtler <floe@butterbrot.org>
2019-12-23 23:41:06 +0100

    Update gst-sink.sh

--- scripts/gst-sink.sh
+++ scripts/gst-sink.sh
@@ -1,4 +1,4 @@
-gst-launch-1.0 -ve udpsrc port=5000 ! tsparse ! tsdemux name=mux \
-  mux. ! queue max-size-buffers=0 max-size-bytes=0 ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
-  mux. ! queue max-size-buffers=0 max-size-bytes=0 ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
-	mux. ! queue max-size-buffers=0 max-size-bytes=0 ! opusdec ! autoaudiosink sync=false
+gst-launch-1.0 -ve udpsrc port=5000 ! application/x-rtp, media=video, clock-rate=90000, encoding-name=MP2T ! rtpmp2tdepay ! tsparse ! tsdemux name=mux \
+	mux. ! queue leaky=downstream ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
+	mux. ! queue leaky=downstream ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
+	mux. ! queue leaky=downstream ! opusdec ! autoaudiosink sync=false
