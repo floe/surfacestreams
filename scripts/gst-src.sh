bb08cd4f2085943861e736093dcc07333951ef20
Florian Echtler <floe@butterbrot.org>
2020-02-04 17:03:58 +0100

    works!

--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -1,4 +1,4 @@
 gst-launch-1.0 -ve mpegtsmux name=mux ! rtpmp2tpay ! udpsink host=$1 port=5000 \
-	v4l2src      ! video/x-raw,width=1280,height=720,framerate=11/1 ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
-	videotestsrc is-live=true ! video/x-raw,width=1280,height=720,framerate=15/1 ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
-	pulsesrc     ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux.
+	v4l2src                   ! video/x-raw,width=1280,height=720                ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
+	videotestsrc is-live=true ! video/x-raw,width=1280,height=720,framerate=10/1 ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
+	pulsesrc                  ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux.
