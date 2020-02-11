d0d5dc6d51460fcbfefd2ac37bcfdc37049d1c29
Florian Echtler <floe@butterbrot.org>
2020-02-11 15:34:05 +0100

    playback of recorded stream

---
+++ scripts/gst-play.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 -ve filesrc location=20200211-152032.mpeg ! tsparse ! tsdemux name=demux  demux.video_0_0042 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink
