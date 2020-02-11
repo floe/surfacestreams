72ad4cb65cf29c47e00d9e60665b1454646365a0
Florian Echtler <floe@butterbrot.org>
2020-02-11 16:47:57 +0100

    update playback script

--- scripts/gst-play.sh
+++ scripts/gst-play.sh
@@ -1,1 +1,5 @@
-gst-launch-1.0 -ve filesrc location=20200211-152032.mpeg ! tsparse ! tsdemux name=demux  demux.video_0_0042 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink
+#!/bin/bash
+gst-launch-1.0 -ve filesrc location=20200211-152032.mpeg ! tsparse ! tsdemux name=demux \
+	demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink \
+	demux.audio_0_0043 ! queue ! opusparse ! opusdec ! autoaudiosink \
+	#demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
