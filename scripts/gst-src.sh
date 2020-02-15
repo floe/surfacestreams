ee2d97127bad18f9c5f17508bdd948ca04dbcc84
Florian Echtler <floe@butterbrot.org>
2020-02-15 08:38:07 +0100

    make recording configurable

--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+[ "$1" = "-r" ] && {
+	record="fork. ! queue ! filesink location=$(date '+%Y%m%d-%H%M%S').mpeg"
+	shift
+}
 gst-launch-1.0 -ve mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=$1 port=5000 \
 	v4l2src                   ! video/x-raw,width=1280,height=720                ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
 	videotestsrc is-live=true ! video/x-raw,width=1280,height=720,framerate=10/1 ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
 	pulsesrc                  ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux. \
-	fork. ! queue ! filesink location="$(date '+%Y%m%d-%H%M%S').mpeg"
+	$record
