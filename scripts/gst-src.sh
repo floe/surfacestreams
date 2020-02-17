a9d795fd30c86f3c8dff94bb2354f3e5308d0ec2
Florian Echtler <floe@butterbrot.org>
2020-02-17 13:43:19 +0100

    adapt to plain webcam

--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -3,8 +3,12 @@
 	record="fork. ! queue ! filesink location=$(date '+%Y%m%d-%H%M%S').mpeg"
 	shift
 }
-gst-launch-1.0 -ve mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=$1 port=5000 \
-	v4l2src                   ! video/x-raw,width=1280,height=720                ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
-	videotestsrc is-live=true ! video/x-raw,width=1280,height=720,framerate=10/1 ! videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! mux. \
-	pulsesrc                  ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux. \
-	$record
+
+videoenc="videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15"
+command="                              ! video/x-raw,width=1280,height=720,framerate=10/1 ! $videoenc ! mux. \
+	v4l2src device=/dev/video-face ! video/x-raw,width=960,height=720,framerate=10/1  ! $videoenc ! mux. \
+	pulsesrc                       ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux. \
+	mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=$1 port=5000 $record"
+
+#gst-launch-1.0 -ve videotestsrc is-live=true $command
+../webcam /dev/video-surf "$command"
