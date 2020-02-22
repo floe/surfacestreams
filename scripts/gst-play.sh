8430bd2a82bcc81a7249a44af31e82c6f499a6ea
Florian Echtler <floe@butterbrot.org>
2020-02-22 21:53:22 +0100

    found a tweak for playback

--- scripts/gst-play.sh
+++ scripts/gst-play.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gst-launch-1.0 -ve filesrc location=20200211-152032.mpeg ! tsparse ! tsdemux name=demux \
-	demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink \
-	demux.audio_0_0043 ! queue ! opusparse ! opusdec ! autoaudiosink \
-	#demux.video_0_0041 ! queue ! h264parse ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false \
+gst-launch-1.0 -ve filesrc location=$1 ! tsparse ! tsdemux name=demux \
+	demux.video_0_0042 ! h264parse ! queue ! avdec_h264 ! videoconvert ! fpsdisplaysink \
+	demux.video_0_0041 ! h264parse ! queue ! avdec_h264 ! videoconvert ! fpsdisplaysink \
+	demux.audio_0_0043 ! opusparse ! queue leaky=downstream ! opusdec ! autoaudiosink sync=false
