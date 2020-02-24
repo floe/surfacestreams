150ae5717a72965de3d773be65570436747fbf6e
Florian Echtler <floe@butterbrot.org>
2020-02-24 16:19:18 +0100

    sync audiosink

--- scripts/gst-sink.sh
+++ scripts/gst-sink.sh
@@ -3,6 +3,6 @@ cd "$(dirname $0)"
 gst-launch-1.0 -ve udpsrc port=5000 ! application/x-rtp, media=video, clock-rate=90000, encoding-name=MP2T ! rtpjitterbuffer ! rtpmp2tdepay ! tsparse ! tsdemux name=mux \
 	mux. ! h264parse ! queue leaky=downstream ! avdec_h264 ! videoconvert ! fpsdisplaysink \
 	mux. ! h264parse ! queue leaky=downstream ! avdec_h264 ! videoconvert ! fpsdisplaysink \
-	mux. ! queue leaky=downstream ! opusdec ! autoaudiosink sync=false &
+	mux. ! opusparse ! queue leaky=downstream ! opusdec ! autoaudiosink &
 ./windowfix.sh
 sleep 100d
