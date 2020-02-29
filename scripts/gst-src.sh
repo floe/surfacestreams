7cd68c5caecd8576a19788270874a9614aa6666b
Florian Echtler <floe@butterbrot.org>
2020-02-29 21:31:04 +0100

    better timestamping in src

--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -2,17 +2,22 @@
 
 cd "$(dirname $0)"
 
+queue="queue max-size-time=200000000 leaky=downstream"
+# filesink _might_ need async=false if it blocks?
+
 [ "$1" = "-r" ] && {
-	record="fork. ! queue ! filesink location=$(date '+%Y%m%d-%H%M%S').mpeg"
+	record="fork. ! $queue ! filesink location=$(date '+%Y%m%d-%H%M%S').mpeg"
 	shift
 }
 
-videoenc="videoconvert ! queue leaky=downstream ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15"
-videoenc2="videoconvert ! queue leaky=downstream ! vaapih264enc bitrate=2000 keyframe-period=10"
-command="                              ! video/x-raw,width=1280,height=720,framerate=10/1 ! $videoenc ! mux. \
-	v4l2src device=/dev/video-face ! video/x-raw,width=960,height=720,framerate=10/1  ! $videoenc2 ! mux. \
-	pulsesrc                       ! audio/x-raw,channels=1,rate=8000                 ! queue leaky=downstream ! opusenc bitrate=8000 ! mux. \
-	mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=$1 port=5000 $record"
+videoenc1="videoconvert ! $queue ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=2 key-int-max=15"
+videoenc2="videoconvert ! $queue ! vaapih264enc bitrate=2000 keyframe-period=10"
+audioenc0="$queue ! opusenc bitrate=8000"
 
-#gst-launch-1.0 -ve videotestsrc is-live=true $command
+command="                                           ! video/x-raw,width=1280,height=720,framerate=10/1 ! $videoenc1 ! mux. \
+  v4l2src  do-timestamp=true device=/dev/video-surf ! video/x-raw,width=960,height=720,framerate=10/1  ! $videoenc2 ! mux. \
+  pulsesrc do-timestamp=true                        ! audio/x-raw,channels=1,rate=16000                ! $audioenc0 ! mux. \
+  mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=$1 port=5000 $record"
+
+#gst-launch-1.0 -ve videotestsrc do-timestamp=true is-live=true $command
 ../webcam /dev/video-surf "$command"
