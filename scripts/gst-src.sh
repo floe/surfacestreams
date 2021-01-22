e141900ce7fbc29ba80c9b55d56a24743ee44a7c
Florian Echtler <floe@butterbrot.org>
2021-01-22 19:55:52 +0100

    low-latency ffplay test

--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for sanity check, run:
-# ffplay -i rtp://127.0.0.1:5000/
+# ffplay -fflags nobuffer -flags low_delay -i rtp://127.0.0.1:5000/
 
 cd "$(dirname $0)"
 
@@ -16,11 +16,11 @@ queue="queue max-size-time=200000000 leaky=downstream"
 videoenc1="videoconvert ! $queue ! x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! video/x-h264,profile=baseline"
 videoenc2="videoconvert ! $queue ! x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! video/x-h264,profile=baseline"
 
-audioenc0="$queue ! opusenc bitrate=8000"
+audioenc0="$queue ! opusenc bitrate=16000"
 
 command="                                           ! videorate ! video/x-raw,width=1280,height=720,framerate=10/1 ! $videoenc1 ! mux. \
   v4l2src  do-timestamp=true device=/dev/video-face ! videorate ! video/x-raw,width=640,height=480,framerate=10/1  ! $videoenc2 ! mux. \
-  pulsesrc do-timestamp=true                        ! audio/x-raw,channels=1,rate=16000                ! $audioenc0 ! mux. \
+  pulsesrc do-timestamp=true                                    ! audio/x-raw,channels=1,rate=16000                ! $audioenc0 ! mux. \
   mpegtsmux name=mux ! tee name=fork ! rtpmp2tpay ! udpsink host=${1:-127.0.0.1} port=5000 $record"
 
 gst-launch-1.0 -vtc videotestsrc do-timestamp=true is-live=true $command
