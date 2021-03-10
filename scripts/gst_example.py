fa8dc01c6263cdfb5dc38269842be1265ea71734
Florian Echtler <floe@butterbrot.org>
2021-03-10 19:16:38 +0100

    cleanup example code

--- scripts/gst_example.py
+++ scripts/gst_example.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 
-import gi
+import sys,gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstBase', '1.0')
-gi.require_version('Gtk', '3.0')
-from gi.repository import GObject, Gst, GstBase, Gtk, GObject
+gi.require_version('GLib', '2.0')
+from gi.repository import Gst, GstBase, GLib
 
-class Main:
-	def __init__(self):
-		Gst.init(None)
+def main(args):
 
-		#self.pipeline = Gst.Pipeline("mypipeline")
-		self.pipeline = Gst.Pipeline()
+    Gst.init(None)
 
-		self.audiotestsrc = Gst.ElementFactory.make("audiotestsrc", "audio")
-		self.pipeline.add(self.audiotestsrc)
+    pipeline = Gst.Pipeline()
 
-		self.sink = Gst.ElementFactory.make("autoaudiosink", "sink")
-		self.pipeline.add(self.sink)
+    udpsrc = Gst.ElementFactory.make("audiotestsrc", "audio")
+    pipeline.add(udpsrc)
 
-		self.audiotestsrc.link(self.sink)
+    sink = Gst.ElementFactory.make("autoaudiosink", "sink")
+    pipeline.add(sink)
 
-		self.pipeline.set_state(Gst.State.PLAYING)
+    udpsrc.link(sink)
 
-start=Main()
-Gtk.main()
+    pipeline.set_state(Gst.State.PLAYING)
+
+    loop = GLib.MainLoop()
+    try:
+        loop.run()
+    except:
+        pass
+
+    pipeline.set_state(Gst.State.NULL)
+
+sys.exit(main(sys.argv))
+
+
+#queue="queue max-size-time=200000000 leaky=upstream"
+#gst-launch-1.0 -vtc udpsrc port=5000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=MP2T ! rtpjitterbuffer do-lost=true ! rtpmp2tdepay ! tsparse set-timestamps=true ! tsdemux name=mux \
+#	mux. ! opusparse ! $queue ! opusdec plc=true ! autoaudiosink \
+#	mux. ! h264parse ! $queue ! avdec_h264 ! videoconvert ! fpsdisplaysink \
+#	mux. ! h264parse ! $queue ! avdec_h264 ! videoconvert ! fpsdisplaysink
