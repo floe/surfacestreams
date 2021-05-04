c3dcd9c17af4881c5ef876bf79ecd3b09455f8fb
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-04 21:13:33 +0200

    adding build option and doc

--- k4a.cpp
+++ k4a.cpp
@@ -1,34 +1,51 @@
-// based on: https://github.com/microsoft/Azure-Kinect-Sensor-SDK/blob/develop/examples/viewer/opengl/main.cpp
-// based on: https://github.com/microsoft/Azure-Kinect-Sensor-SDK/blob/develop/examples/transformation/main.cpp
+// based on:
+// https://github.com/microsoft/Azure-Kinect-Sensor-SDK/blob/develop/examples/viewer/opengl/main.cpp
+// based on:
+// https://github.com/microsoft/Azure-Kinect-Sensor-SDK/blob/develop/examples/transformation/main.cpp
 // tested with: libk4a1.4-1.4.1
 
-#include <iostream>
 #include "KinectAzure.h"
-#include "logger.h"
 #include "timer.h"
+#include <iostream>
 
-int main( int argc, char* argv [] ) {
+// to enable glog logging, set LOG = 1
+// CAVEAT: If logging enabled, build project
+//         using using CMakeLists to pull in
+//         glog dependencies
+//
+#define LOG 0
+#if LOG == 1
+#include "logger.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+#if LOG == 1
     logger(argc, argv);
+#endif
 
-	const char* gstpipe = nullptr;
-	if (argc >= 2) gstpipe = argv[1];
+    const char* gstpipe = nullptr;
+    if (argc >= 2)
+        gstpipe = argv[1];
 
-	KinectAzure cam(gstpipe);
+    KinectAzure cam(gstpipe);
 
-    LOG(INFO) << "-- chroma keying";
-	while (!cam.do_quit) {
+    while (!cam.do_quit) {
 
-		cam.retrieve_frames();
+        cam.retrieve_frames();
 
-		if (cam.find_plane) {
-		    Timer timer;
-			cam.ransac_plane();
-			LOG(INFO) << "-- segmented plane in " << timer.getDuration() << " ms";
-			cam.find_plane = false;
-		}
+        if (cam.find_plane) {
+            Timer timer;
+            cam.ransac_plane();
+#if LOG == 1
+            LOG(INFO) << "-- segmented plane in " << timer.getDuration()
+                      << " ms";
+#endif
+            cam.find_plane = false;
+        }
 
-		cam.remove_background();
+        cam.remove_background();
 
-		cam.send_buffer();
-	}
+        cam.send_buffer();
+    }
 }
