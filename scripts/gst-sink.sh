c05c6254371096af0b47d2545a612c924f1631f7
Florian Echtler <floe@butterbrot.org>
2021-05-22 12:54:10 +0200

    some pipeline optimizations

--- scripts/gst-sink.sh
+++ scripts/gst-sink.sh
@@ -4,7 +4,7 @@ cd "$(dirname $0)"
 queue="queue max-size-time=200000000 leaky=upstream"
 # maybe add rtpssrcdemux between jitterbuf and mp2ts-depay
 
-gst-launch-1.0 -vtc udpsrc port=5000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=MP2T ! rtpjitterbuffer do-lost=true ! rtpmp2tdepay ! tsparse set-timestamps=true ! tsdemux name=mux \
+gst-launch-1.0 -vtc udpsrc port=5000 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=MP2T ! rtpjitterbuffer do-lost=true ! rtpmp2tdepay ! tsdemux name=mux \
 	mux. ! opusparse ! $queue ! opusdec plc=true ! autoaudiosink \
 	mux. ! h264parse ! $queue ! avdec_h264 ! videoconvert ! fpsdisplaysink \
 	mux. ! h264parse ! $queue ! avdec_h264 ! videoconvert ! fpsdisplaysink
--- scripts/gst-src.sh
+++ scripts/gst-src.sh
@@ -19,7 +19,7 @@ facesrc="videotestsrc is-live=true"
 	shift ; shift
 }
 
-videoenc="videoconvert ! $queue ! x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! video/x-h264,profile=baseline"
+videoenc="videoconvert ! queue max-size-buffers=1 ! x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency byte-stream=true threads=2 key-int-max=15 ! video/x-h264,profile=baseline ! queue max-size-time=100000000"
 audioenc="$queue ! opusenc bitrate=16000"
 
 command="                    ! videorate ! video/x-raw,width=1280,height=720,framerate=10/1 ! $videoenc ! mux. \
