3a5ae4dce98dcd4eaffb92921494314b04042efa
Florian <floe@butterbrot.org>
2022-01-19 12:44:08 +0100

    minor fixes, cleanup

--- scripts/start-virtual-src.sh
+++ scripts/start-virtual-src.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-MODE="v4l2"
-CAM="$1"
-
-if [ "$CAM" = "" ] ; then
-	MODE="virtcam"
-	CAM="null"
-fi
-
-#../webcam $MODE $CAM "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video20"
-../realsense "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video20"
+cd ../
+./surfacecast $1 $2 "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video20"
