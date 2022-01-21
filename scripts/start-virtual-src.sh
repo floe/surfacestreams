14fa94ccfea23f244abd97b18a719047f99e5f2a
Florian Echtler <floe@butterbrot.org>
2022-01-21 14:34:33 +0100

    disable autofocus for surface cam

--- scripts/start-virtual-src.sh
+++ scripts/start-virtual-src.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+v4l2-ctl -d /dev/video-surf -c focus_auto=0
+v4l2-ctl -d /dev/video-surf -c focus_absolute=0
+
 cd ../
 ./surfacecast $1 $2 "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video20"
