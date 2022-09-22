ee042750dd02f45606e648fd8e005b90964243a3
Florian Echtler <floe@butterbrot.org>
2022-09-22 15:26:30 +0200

    add example code for shmsink/shmsrc (not working with surfacestreams yet)

--- scripts/start-virtual-sink.sh
+++ scripts/start-virtual-sink.sh
@@ -1,1 +1,2 @@
 gst-launch-1.0 -vct v4l2src device=/dev/video20 ! videoconvert ! fpsdisplaysink
+#gst-launch-1.0 shmsrc socket-path=/tmp/blah is-live=true do-timestamp=true ! video/x-raw,format=RGB,width=1280,height=720,framerate=15/1 ! videoconvert ! fpsdisplaysink
--- scripts/start-virtual-src.sh
+++ scripts/start-virtual-src.sh
@@ -4,4 +4,7 @@ v4l2-ctl -d /dev/video-surf -c focus_auto=0
 v4l2-ctl -d /dev/video-surf -c focus_absolute=0
 
 cd ../
+rm /tmp/blah*
 ./surfacecast $1 $2 "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! v4l2sink device=/dev/video20"
+#./surfacecast  $1 $2 "videoconvert ! video/x-raw,format=RGB,width=1280,height=720 ! shmsink socket-path=/tmp/blah wait-for-connection=false"
+rm /tmp/blah*
