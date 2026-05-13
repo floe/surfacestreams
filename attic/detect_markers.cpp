9431610af692f704f354d52fa9ee1253f0f32366
Florian Echtler <floe@butterbrot.org>
2026-05-13 22:34:50 +0200

    clean out example code

--- attic/detect_markers.cpp
+++ attic/detect_markers.cpp
@@ -3,7 +3,6 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/objdetect/aruco_detector.hpp>
 #include <iostream>
-#include "aruco_samples_utility.hpp"
 
 using namespace std;
 using namespace cv;
@@ -44,11 +43,9 @@ int main(int argc, char *argv[]) {
     parser.about(about);
 
     bool showRejected = parser.has("r");
-    bool estimatePose = parser.has("c");
-    float markerLength = parser.get<float>("l");
 
-    aruco::DetectorParameters detectorParams = readDetectorParamsFromCommandLine(parser);
-    aruco::Dictionary dictionary = readDictionatyFromCommandLine(parser);
+    aruco::DetectorParameters detectorParams;
+    aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(aruco::DICT_4X4_50);
 
     if (parser.has("refine")) {
         // override cornerRefinementMethod read from config file
@@ -63,8 +60,6 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Corner refinement method: " << refineMethods[detectorParams.cornerRefinementMethod] << std::endl;
 
-    int camId = parser.get<int>("ci");
-
     String video;
     if(parser.has("v")) {
         video = parser.get<String>("v");
@@ -75,13 +70,6 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    //! [aruco_pose_estimation1]
-    Mat camMatrix, distCoeffs;
-    if(estimatePose) {
-        // You can read camera parameters from tutorial_camera_params.yml
-        readCameraParamsFromCommandLine(parser, camMatrix, distCoeffs);
-    }
-    //! [aruco_pose_estimation1]
     //! [aruco_detect_markers]
     cv::aruco::ArucoDetector detector(dictionary, detectorParams);
     cv::VideoCapture inputVideo;
@@ -96,15 +84,6 @@ int main(int argc, char *argv[]) {
 
     double totalTime = 0;
     int totalIterations = 0;
-
-    //! [aruco_pose_estimation2]
-    // set coordinate system
-    cv::Mat objPoints(4, 1, CV_32FC3);
-    objPoints.ptr<Vec3f>(0)[0] = Vec3f(-markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[1] = Vec3f(markerLength/2.f, markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[2] = Vec3f(markerLength/2.f, -markerLength/2.f, 0);
-    objPoints.ptr<Vec3f>(0)[3] = Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
-    //! [aruco_pose_estimation2]
 
     while(inputVideo.grab()) {
 	cv::Mat raw, image, imageCopy;
@@ -123,13 +102,6 @@ int main(int argc, char *argv[]) {
         size_t nMarkers = corners.size();
         vector<Vec3d> rvecs(nMarkers), tvecs(nMarkers);
 
-        if(estimatePose && !ids.empty()) {
-            // Calculate pose for each marker
-            for (size_t i = 0; i < nMarkers; i++) {
-                solvePnP(objPoints, corners.at(i), camMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-            }
-        }
-        //! [aruco_pose_estimation3]
         double currentTime = ((double)getTickCount() - tick) / getTickFrequency();
         totalTime += currentTime;
         totalIterations++;
@@ -137,18 +109,11 @@ int main(int argc, char *argv[]) {
             cout << "Detection Time = " << currentTime * 1000 << " ms "
                  << "(Mean = " << 1000 * totalTime / double(totalIterations) << " ms)" << endl;
         }
-        //! [aruco_draw_pose_estimation]
         // draw results
         image.copyTo(imageCopy);
         if(!ids.empty()) {
             cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-
-            if(estimatePose) {
-                for(unsigned int i = 0; i < ids.size(); i++)
-                    cv::drawFrameAxes(imageCopy, camMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 1.5f, 2);
-            }
         }
-        //! [aruco_draw_pose_estimation]
 
         if(showRejected && !rejected.empty())
             cv::aruco::drawDetectedMarkers(imageCopy, rejected, noArray(), Scalar(100, 0, 255));
