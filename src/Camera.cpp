a51746416a97bb530b41b662d5471a7670816474
Florian Echtler <floe@butterbrot.org>
2026-05-29 19:06:32 +0200

    Mat debug helpers

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -9,6 +9,26 @@
 // OpenCV stuff
 //
 
+// courtesy of https://gist.github.com/rummanwaqar/cdaddd5a175f617c0b107d353fd33695
+std::string type2str(int type) {
+  std::string r = "CV_";
+  unsigned char depth = type & CV_MAT_DEPTH_MASK;
+  unsigned char chans = '1' + (type >> CV_CN_SHIFT);
+  switch (depth) {
+    case CV_8U:  r +=  "8U"; break;
+    case CV_8S:  r +=  "8S"; break;
+    case CV_16U: r += "16U"; break;
+    case CV_16S: r += "16S"; break;
+    case CV_32S: r += "32S"; break;
+    case CV_32F: r += "32F"; break;
+    case CV_64F: r += "64F"; break;
+    default:     r += "???"; break;
+  }
+  return (r + "C") += chans;;
+}
+
+#define describe_mat(mat) #mat << " type:" << type2str(mat.type()) << " ch:" << mat.channels() << " el:" << mat.elemSize() << " dim:" << mat.size() << " step:" << mat.step << " total:" << mat.total()
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
